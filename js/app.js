14e64843f51231a2ba3ffa691912bc4a27cb0c1c
ju won bae <juwonbae@juui-MacBookAir.local>
2026-02-06 13:52:29 +0900

    Complete Revert: Remove Personalization Logic and Styles v112

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 
 
-import { renderHome } from './home.js?v=111';
-import { renderMeeting } from './meeting.js?v=111';
-import { renderFuneral } from './funeral.js?v=111';
-import { renderCommunity } from './community.js?v=111';
-import { renderVideoDetail } from './video_detail.js?v=111';
+import { renderHome } from './home.js?v=112';
+import { renderMeeting } from './meeting.js?v=112';
+import { renderFuneral } from './funeral.js?v=112';
+import { renderCommunity } from './community.js?v=112';
+import { renderVideoDetail } from './video_detail.js?v=112';
 import { renderVideoDetail } from './video_detail.js?v=111';
 
 // State
